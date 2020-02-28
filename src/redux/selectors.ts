f4bbb0829abf335da63c9be1d7c67dd3b50f7343
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-02-27 22:21:55 -0700

    Mock Pawn movePositions class method

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,13 +1,45 @@
 import { AppState } from "../store";
 import { createSelector } from "reselect";
 import { A1, A2, A3, A4 } from "../constants/positions";
+import { IPiece, Pieces } from "../constants/pieces";
 
-export const selectPieces = (state: AppState) => state.game.pieces
-export const selectPiece = (state: AppState, position: string) => state.game.pieces.find(piece => piece.position === position)
-export const selectMovingPiece = (state: AppState) => state.game.movingPiece
+export const selectPieces = (state: AppState) => state.game.pieces;
+export const selectPiece = (state: AppState, position: string) =>
+  state.game.pieces.find(piece => piece.position === position);
+export const selectMovingPiece = (state: AppState) => state.game.movingPiece;
 
 export const selectValidPositions = createSelector(
-    selectPieces,
-    selectMovingPiece,
-    (pieces, movingPiece) => [A3, A4]
-)
+  selectPieces,
+  selectMovingPiece,
+  (pieces, movingPiece) =>
+    movingPiece ? PieceFactory.fromPiece(movingPiece).movePositions() : []
+);
+
+class PieceFactory {
+  static fromPawn(pawn: IPiece) {
+    new Pawn(pawn);
+  }
+
+  static fromPiece(piece: IPiece) {
+    switch (piece.type) {
+      case Pieces.pawn:
+        return new Pawn(piece);
+      default:
+        throw new Error();
+        break;
+    }
+  }
+}
+
+class Piece {
+  constructor(public piece: IPiece) {}
+  movePositions() {
+    throw new Error("NYI");
+  }
+}
+
+class Pawn extends Piece {
+  movePositions() {
+    return [A3, A4];
+  }
+}
